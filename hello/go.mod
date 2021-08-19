c9d94a5efd9881f5228aff899f414ee24b6b350a
w3aman <aman.gupta@mayadata.io>
2021-08-19 17:03:34 +0530

    first go-code
    Signed-off-by: w3aman <aman.gupta@mayadata.io>

---
+++ hello/go.mod
@@ -0,0 +1,5 @@
+module github.com/w3aman/mygo
+
+go 1.15
+
+require rsc.io/quote v1.5.2
---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("Welcome, on board")
+
+	fmt.Println(quote.Go())
+}
