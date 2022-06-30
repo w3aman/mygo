b7575023ad77935972420a59eab905f11cdeff2a
Aman Gupta <aman.gupta@mayadata.io>
2022-06-30 17:55:09 +0530

    Update cncf_k8s_install.sh

--- cncf_k8s_install.sh
+++ cncf_k8s_install.sh
@@ -6,6 +6,8 @@ sed -e '/swap/s/^/#/g' -i /etc/fstab
 
 swapoff -a
 
+apt-get update && apt-get -o Dpkg::Options::="--force-confold" upgrade -q -y --force-yes &&
+
 apt-get update && apt-get install -y apt-transport-https curl
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -22,3 +24,18 @@ apt-mark hold kubelet kubeadm kubectl
 sed -i -e 's/containerd.sock/containerd.sock --exec-opt native.cgroupdriver=systemd/g' /lib/systemd/system/docker.service 
 systemctl daemon-reload 
 systemctl restart docker
+
+if ["$1" == "master"]; then
+# export MASTER_IP=$2
+kubeadm init --apiserver-advertise-address=$2 --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+else
+echo "installation of k8s on nodes successfully done!"
+fi
+
